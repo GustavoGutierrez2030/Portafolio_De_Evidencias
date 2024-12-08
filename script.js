00bd4a2eddbadcad1e234baa610c54abeaf02e20
GustavoGutierrez2030 <gustavo.sonic2003@hotmail.com>
2024-12-08 17:59:07 -0600

    Feat Prueba de app
    Se agrego la aplicacion al servidor

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleTopics(topicId) {
+  const topicList = document.getElementById(topicId);
+  if (topicList) {
+    topicList.classList.toggle("hidden");
+  } else {
+    console.error(`No se encontró el elemento con ID: ${topicId}`);
+  }
+}
+
+function loadPDF(pdfPath) {
+  const pdfFrame = document.getElementById('pdf-frame');
+  if (pdfFrame) {
+    pdfFrame.src = `pdfs/${pdfPath}`;
+  } else {
+    console.error("El iframe no se encontró.");
+  }
+}
