a95275ebc84f5e09e477f7968fb400b446ae18d9
GustavoGutierrez2030 <gustavo.sonic2003@hotmail.com>
2024-12-09 17:43:56 -0600

    Feat Last Updates
    Se agragaron todas las actividades y tambien se modificaron los estilos

--- script.js
+++ script.js
@@ -1,47 +1,33 @@
 function enterPortfolio() {
-    const coverPage = document.getElementById('cover-page');
-    const mainContent = document.querySelector('.container');
-  
-    coverPage.style.display = 'none';
-    mainContent.style.display = 'flex';
-  
-    // Mostrar el botón para sidebar
-    const toggleSidebarButton = document.querySelector('.toggle-sidebar');
-    toggleSidebarButton.classList.add('visible');
+  // Redirigir a la página del portafolio
+  window.location.href = "portafolio.html";
+}
+
+function toggleTopics(topicId) {
+  const topicList = document.getElementById(topicId);
+  if (topicList) {
+    topicList.classList.toggle('hidden');
   }
-  
-  function toggleTopics(topicId) {
-    const topicList = document.getElementById(topicId);
-    if (topicList) {
-      topicList.classList.toggle('hidden');
-    }
+}
+
+function loadPDF(pdfPath) {
+  const pdfFrame = document.getElementById('pdf-frame');
+  if (pdfFrame) {
+    pdfFrame.src = `pdfs/${pdfPath}`;
+  } else {
+    console.error("El iframe no se encontró.");
   }
-  
-  function loadPDF(pdfPath) {
-    const pdfFrame = document.getElementById('pdf-frame');
-    const sidebar = document.querySelector('.sidebar');
-    const mainContent = document.querySelector('.main-content');
-    const toggleSidebarButton = document.querySelector('.toggle-sidebar');
-  
-    if (pdfFrame) {
-      pdfFrame.src = `pdfs/${pdfPath}`;
-  
-      // Maximizar PDF
-      sidebar.classList.add('hidden');
-      mainContent.classList.add('fullscreen');
-  
-      // Ocultar el botón de sidebar
-      toggleSidebarButton.classList.remove('visible');
-    } else {
-      console.error("El iframe no se encontró.");
-    }
-  }
-  
-  function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    const mainContent = document.querySelector('.main-content');
-  
-    sidebar.classList.toggle('hidden');
-    mainContent.classList.toggle('fullscreen');
-  }
-  
+}
+
+function toggleSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  const mainContent = document.querySelector('.main-content');
+  sidebar.classList.toggle('hidden');
+  mainContent.style.width = sidebar.classList.contains('hidden') ? '100%' : '80%';
+}
+
+function goBackToHome() {
+  // Aquí puedes redirigir al usuario a la página principal (portada)
+  window.location.href = 'index.html'; // Cambia 'index.html' por la ruta de tu página principal si es diferente
+}
+
