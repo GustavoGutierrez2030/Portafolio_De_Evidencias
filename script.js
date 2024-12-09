f32641de0386fdca421580fa0a8ce449a9dfeee4
GustavoGutierrez2030 <gustavo.sonic2003@hotmail.com>
2024-12-08 23:56:57 -0600

    Feat Mejora visual

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-function toggleTopics(topicId) {
-  const topicList = document.getElementById(topicId);
-  if (topicList) {
-    topicList.classList.toggle("hidden");
-  } else {
-    console.error(`No se encontró el elemento con ID: ${topicId}`);
+function enterPortfolio() {
+    const coverPage = document.getElementById('cover-page');
+    const mainContent = document.querySelector('.container');
+  
+    coverPage.style.display = 'none';
+    mainContent.style.display = 'flex';
   }
-}
-
-function loadPDF(pdfPath) {
+  
+  function toggleTopics(topicId) {
+    const topicList = document.getElementById(topicId);
+    if (topicList) {
+      topicList.classList.toggle('hidden');
+    }
+  }
+  
+  function loadPDF(pdfPath) {
     const pdfFrame = document.getElementById('pdf-frame');
     if (pdfFrame) {
-      pdfFrame.src = `/Portafolio_De_Evidencias/pdfs/${pdfPath}`;
+      pdfFrame.src = `pdfs/${pdfPath}`;
     } else {
       console.error("El iframe no se encontró.");
     }
-  }
-
-  function enterPortfolio() {
-    const coverPage = document.getElementById('cover-page');
-    const mainContent = document.getElementById('main-content');
-  
-    // Oculta la portada y muestra el contenido principal
-    coverPage.classList.add('hidden');
-    mainContent.classList.remove('hidden');
   }
   
